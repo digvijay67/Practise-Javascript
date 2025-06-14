b8f883eb98ec1e7422f3091f2d65561796597acf
digvijay67 <digvijaysingrajput67@gmail.com>
2025-06-14 16:45:39 +0530

    mainobject

---
+++ Basic/DatatypeConfu.js
@@ -0,0 +1,15 @@
+let Score = "55adv";
+
+ console.log(typeof Score);
+
+ let ValueInNumber = Number(Score);
+ console.log(typeof ValueInNumber);
+ console.log(ValueInNumber);
+
+
+  let isLoggedIn = true
+  let BooleanIsLoggedIn = Boolean(isLoggedIn);
+  
+
+  console.log(BooleanIsLoggedIn);
+  
