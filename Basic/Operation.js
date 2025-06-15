95db832f8d450d0c01056912633578a4d8aa8a87
digvijay67 <digvijaysingrajput67@gmail.com>
2025-06-15 13:44:29 +0530

    primitivedatatype

---
+++ Basic/Operation.js
@@ -0,0 +1,18 @@
+let a  = 3 ;
+let b = a++;
+
+console.log(`a:${a},b:${b}`);
+
+let c = 3;
+let v = ++c;
+
+console.log(`c:${c},v:${v}`);
+
+console.log("hello"!= "hello");
+console.log(1 != 4);
+console.log( true != false);
+console.log( c != v);
+
+
+
+let you = "yash"
---
+++ Basic/Primitive.js
@@ -0,0 +1,34 @@
+// primitive type
+// 7 type: String,number boolean null bignit undefined Symbol
+
+const score = 100
+const scorevalue = 100.6
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id===anotherId);
+
+//const bigNumber = 23344455566445666n;
+
+
+
+// non primitive 
+ // Array, Object, function,
+
+ const heroes = ["Sori","angara","yah"];
+ let myObj ={
+    name:"Yash",
+    age: 22,
+ }
+ // function treat a varaible form
+ 
+
+  const myFunction = function (){
+    console.log("Hello ww");
+    
+
+ }
+ console.log(typeof anotherId)
