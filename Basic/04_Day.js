9ec30ffc537706864f5d260c6f9ccf79ae10adae
digvijay67 <digvijaysingrajput67@gmail.com>
2025-06-16 13:01:14 +0530

    change the file and add

---
+++ Basic/04_Day.js
@@ -0,0 +1,38 @@
+// maths in javascript using a mathematicals operation
+const Score = 600;
+
+const Balance = new Number(299);
+console.log(Balance);
+console.log(`Total Socre is ${Score} and Availble Balance is ${Balance+Score}`);
+console.log(Balance.toString());
+console.log(Balance.toFixed(2));
+const otherNname = 23.655;
+console.log(otherNname.toPrecision(9));
+const Hundreds = 123456789
+console.log(Hundreds.toLocaleString('en-IN'));
+
+
+
+
+//============ Maths using a library tu javascript
+
+console.log(Math.abs(5));  // only positive  value
+console.log(Math.round(6.7)); // overall around value
+console.log(Math.ceil(5.4));  // around value
+console.log(Math.floor(1.9));  // lowest value
+ 
+console.log((Math.random()*10) +1);
+console.log(Math.floor(Math.random()*10) + 1);
+console.log(Math.random());
+
+const min = 10;
+const max  = 20;
+console.log(Math.floor(Math.random()* (max-min+1)+ min))   // important notes
+
+
+
+
+
+
+ // abs is a absulute value
+
