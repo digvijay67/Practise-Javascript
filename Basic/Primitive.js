08b92aa1ef40e6f59c8b944d5481b6f339c83189
digvijay67 <digvijaysingrajput67@gmail.com>
2025-06-16 13:01:21 +0530

    adding

--- Basic/Primitive.js
+++ Basic/Primitive.js
@@ -22,7 +22,13 @@ console.log(id===anotherId);
  let myObj ={
     name:"Yash",
     age: 22,
+    
+    
  }
+ console.log(myObj);
+ 
+ console.log(heroes===myObj);
+ 
  // function treat a varaible form
  
 
@@ -32,3 +38,65 @@ console.log(id===anotherId);
 
  }
  console.log(typeof anotherId)
+
+
+ // stack using a primitive data type
+ // heap using a non primitive data type
+
+ let myName = "Yash"
+
+ let userOne ={
+    email: "digvijay67@gmail.com",
+
+    upiId: "9888101543@ybl",
+    mobileNumber: "9881017423"
+ }
+ let UserTwo = userOne
+
+ UserTwo.email= "digvijay78@gmail.com"
+
+ console.log(userOne.email);
+ console.log(UserTwo.email);
+
+  // changes using a original value
+    
+// String in Javascript date 15-06-2025
+
+const name = "Yash";
+let namee = "rom"
+const repo = 60;
+
+console.log(`Hello my name is ${name} and my repo is ${repo}`);
+
+let gameName = new String("Digvijaysing")
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+console.log(gameName.length);
+console.log(gameName.toUpperCase())
+console.log(gameName.charAt(3)); 
+console.log(namee.replace("rom","yash")); 
+
+
+let nam = namee.replace("rom","java")
+console.log(nam);
+
+const newwString = gameName.substring(-9,6)  // substring are a ignore a reverse value and call a simple value negative value not a call only postitive integer call
+console.log(newwString);
+
+const newString = gameName.slice(-9,6)  // slice is a reverse value are call and  regular value are both operation call a "slice"
+console.log(newString);
+
+//trim function using a space and gab are not a use
+ const you = "     Digv     "
+console.log(you);
+console.log(you.trim());
+console.log(gameName.includes('Dighh')); // include is a finding a value and  sting an let and var any datatype and value using a include function
+
+
+const valueName = new String( "Digvijay-Vilassing-Rajput")
+console.log(valueName.split('-'));
+
+
+
+
+
